7a0476d7fc4ba904434eec885ffaa421ff78553f
Thomas Salas <tjsalas18@sbcglobal.net>
2018-06-27 09:12:49 -0400

    moved to homework directory

---
+++ README.md
@@ -0,0 +1,1 @@
+This is part of project 1 for week one of Xtern Bootcamp. The program takes to inputs from a user then it combines, and it adds diffrent style to each. I completly re did my code and if you would like to view more of its progress please refer to the dinosaur directory.
--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const button = document.querySelector('.target')
-const toChange = document.querySelector('#unique')
-const changeText = function(){
-    const myText =  document.querySelector('.input').value
-    
-    toChange.textContent = myText
+const button = document.querySelector('.btn')
+const list = document.querySelector('.dinoList')
+const form = document.querySelector('.favDinosaur')
+
+
+
+
+const addDinoToList = function(ev){
+    ev.preventDefault()
+    const entry = document.createElement('li') 
+    entry.appendChild(dinomaker())
+    entry.appendChild(dinoFeeder())
+    list.appendChild(entry)
 }
-button.addEventListener('click', changeText)
-const fourm = document.querySelector('.fourm')
-fourm.addEventListener('submit', changeText)
+
+const dinomaker = function(){
+    const name = document.querySelector('.dinoName').value
+    const dinoEntry = document.createElement('span')
+    dinoEntry.textContent = name 
+    dinoEntry.classList.add('bold')
+    return dinoEntry
+}
+
+const dinoFeeder = function(){
+    const diet = document.querySelector('.dinoDiet').value
+    let dinoFood = document.createElement('span')
+    dinoFood.textContent =  " and it eats " + diet
+    if(diet == "People" || diet === "people"){
+        dinoFood.textContent += "! How scary."
+    }
+    else{
+        dinoFood.textContent += "."
+    }
+    dinoFood.classList.add('fancy')
+    return dinoFood
+}
+
+
+form.addEventListener('submit',addDinoToList)
