feac6c6511675796312c19a4c410f3a5f0bba6ec
Thomas Salas <tjsalas18@sbcglobal.net>
2018-06-27 23:35:38 -0400

    addes style to input

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 
-const list = document.querySelector('.dinoList')
-form.addEventListener('submit',addDinoToList)
-
-
-
+const userInput = document.querySelector('.favDinosaur') 
 
 
 const addDinoToList = function(ev){
     ev.preventDefault()
-    const form = document.querySelector('.favDinosaur')
-    const entry = document.createElement('li') 
-    entry.appendChild(dinomaker())
-    entry.appendChild(dinoFeeder())
-    list.appendChild(entry)
+    const form = ev.target 
+    const list = document.querySelector('.dinoList')
+    
+    const dinosaur = {
+        species: form.dinoName.value,
+        diet: form.dinoDiet.value
+    }
+    
+    const dinoEntry = createDino(dinosaur)
+    debugger
+    list.appendChild(dinoEntry)
+
     form.reset()
+
 }
 
-const dinomaker = function(){
-    const name = document.querySelector('.dinoName').value
-    const dinoEntry = document.createElement('span')
-    dinoEntry.textContent = name 
-    dinoEntry.classList.add('bold')
-    return dinoEntry
+
+const createDino = function(dinosaur){
+    const entry = document.createElement('li') 
+    const properties = Object.keys(dinosaur)
+    properties.forEach((propertyName) =>{
+        const span = spanBuilder(propertyName, dinosaur[propertyName])
+        entry.appendChild(span)
+    })
+    return entry
 }
 
-const dinoFeeder = function(){
-    const diet = document.querySelector('.dinoDiet').value
-    let dinoFood = document.createElement('span')
-    dinoFood.textContent =  " and it eats " + diet
-    if(diet == "People" || diet === "people"){
-        dinoFood.textContent += "! How scary."
+const spanBuilder = function(name, value){
+    const span = document.createElement('span')
+    span.classList.add(name)
+    if(name === "species"){
+        span.textContent = value + " and it eats "
     }
-    else{
-        dinoFood.textContent += "."
-    }
-    dinoFood.classList.add('fancy')
-    return dinoFood
+    else{span.textContent = value}
+    return span
 }
 
-
+userInput.addEventListener('submit',addDinoToList)
