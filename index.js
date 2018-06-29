49387e411bc50479273e7e2b0fb0fdfeaa6fb8d1
Thomas Salas <tjsalas18@sbcglobal.net>
2018-06-29 07:52:35 -0400

    code has been restructred, favorite button now works, and data will persist

--- index.js
+++ index.js
@@ -1,48 +1,78 @@
 
-const userInput = document.querySelector('.favDinosaur') 
-let dinoArray = []
-dinoArray.forEach((object) =>{
-    tempBtn = document.querySelector('.'+ object.species)
-    tempBtn.addEventListener("click", delInput)
-})
-userInput.addEventListener('submit',addDinoToList)
+class App{
+    constructor(){
+    const userInput = document.querySelector('.favDinosaur') 
+    this.dinoArray = []
+    this.load()
+    userInput.addEventListener('submit', (ev) => {
+        ev.preventDefault()
+        this.addDinoToList(ev)
+    
+    })
+   
+ }
 
-const addDinoToList = function(ev){
-    ev.preventDefault()
+
+save(){
+    localStorage.setItem('dinoArray', JSON.stringify(this.dinoArray))
+}
+load(){
+   const dinoArray = JSON.parse(localStorage.getItem('dinoArray'))
+   
+   if   (dinoArray){
+   dinoArray.forEach(dinosaur => this.createDino(dinosaur))
+}
+}
+
+ addDinoToList(ev){
     const form = ev.target 
-    const list = document.querySelector('.dinoList')
+   
     
     const dinosaur = {
         species: form.dinoName.value,
-        diet: form.dinoDiet.value
+        diet: form.dinoDiet.value,
+        favorite: false,
     }
-    dinoArray.push(dinosaur)
-    const dinoEntry = createDino(dinosaur)
-    list.appendChild(dinoEntry)
+    this.dinoArray.push(dinosaur)
+    this.createDino(dinosaur)
+ 
 
     form.reset()
-
+    this.save()
 }
 
 
-const createDino = function(dinosaur){
+createDino(dinosaur){
     const entry = document.createElement('li') 
     const btn = document.createElement('button')
-
+    const list = document.querySelector('.dinoList')
     entry.classList.add(dinosaur.species)
     btn.classList.add(dinosaur.species)
     const properties = Object.keys(dinosaur)
     properties.forEach((propertyName) =>{
-        const span = spanBuilder(propertyName, dinosaur[propertyName])
+        const span = this.spanBuilder(propertyName, dinosaur[propertyName])
         entry.appendChild(span)
     })
     btn.textContent = "Delete"
     btn.id = dinosaur.species
+    btn.addEventListener('click',(e) =>{ 
+        const f = e.target
+        this.itemDelete(f)
+    })
     entry.appendChild(btn)
-    return entry
+    const favoriteBtn = document.createElement('button')
+    favoriteBtn.textContent = "Favorite"
+    favoriteBtn.classList.add('fav')
+    favoriteBtn.addEventListener('click',() => this.toggleFavorite(dinosaur, entry))
+    entry.appendChild(favoriteBtn)
+    list.appendChild(entry)
 }
+toggleFavorite(dinosaur, entry) {
+    dinosaur.favorite = entry.classList.toggle('fav')
+    this.save()
+  }
 
-const spanBuilder = function(name, value){
+spanBuilder(name, value){
     const span = document.createElement('span')
     span.classList.add(name)
     if(name === "species"){
@@ -52,20 +82,28 @@ const spanBuilder = function(name, value){
     return span
 }
 
-const delInput = function(event){
+delInput(event){
     event.preventDefault()
     idFetcher = event.target.classList.value
     userInput.removeChild(document.querySelector(idFetcher))
+   this.save()
 }
 
-const itemDelete = function(f){
+itemDelete(f){
     const element = f.closest('li')
     const parElment = element.closest('ol')
     parElment.removeChild(element)
     let eraser = 0
-    dinoArray.forEach(function(element){
+    this.dinoArray.forEach(function(element){
         if (element.species == f)
         eraser = dinoArray.findIndex(element)
     })
-    dinoArray.splice (eraser, 1)
+    this.dinoArray.splice (eraser, 1)
+    this.save()
 }
+
+
+
+
+}
+const app = new App()
